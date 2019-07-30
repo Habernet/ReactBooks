fa75e36f0be3fbd25de35705d73473b3e411e027
habernet <phaberern@gmail.com>
2019-07-30 15:25:55 -0400

    Boiler plate code added for models and routes

--- models/book.js
+++ models/book.js
@@ -5,7 +5,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
+  description: String,
+  image: String,
+  link: String,
   date: { type: Date, defualt: Date.now }
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,7 +13,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+//Connect to Mongoose locally or deployed
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactbooks";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+//Use the router
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
